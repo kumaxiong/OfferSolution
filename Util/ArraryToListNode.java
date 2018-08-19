84d71a39fcd75a482327f2a6f65314a7d61e7393
kumaxiong <1009224322@qq.com>
2018-08-19 18:23:23 +0800

    新增题号，gitignore

--- Util/ArraryToListNode.java
+++ Util/ArraryToListNode.java
@@ -3,12 +3,14 @@ package Util;
 import OfferSlution.ListNode;
 
 public class ArraryToListNode {
-    public static ListNode getNode(int[] array){
+
+    public static ListNode getNode(int[] array) {
         ListNode result = new ListNode(array[0]);
         ListNode behind = result;
         ListNode ahead = null;
-        if(array.length == 1)
+        if (array.length == 1) {
             return result;
+        }
         for (int i = 1; i < array.length; i++) {
             ahead = new ListNode(array[i]);
             behind.next = ahead;
